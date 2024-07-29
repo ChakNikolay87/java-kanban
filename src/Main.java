e63b6906500c83d694a58808f7855d2614e2f7b0
ChakNikolay87 <ChakNikolay@yandex.ru>
2024-07-29 15:28:34 +0300

    Created file: Epic, TaskStatus, Task, Manager Subtask, Task, Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager taskManager = new TaskManager();
+
+        Task task1 = taskManager.createTask("Task 1", "Description 1");
+        Task task2 = taskManager.createTask("Task 2", "Description 2");
+
+        Epic epic1 = taskManager.createEpic("Epic 1", "Description 1");
+        Subtask subtask1 = taskManager.createSubtask("Subtask 1", "Description 1", epic1.getId());
+        Subtask subtask2 = taskManager.createSubtask("Subtask 2", "Description 2", epic1.getId());
+
+        Epic epic2 = taskManager.createEpic("Epic 2", "Description 2");
+        Subtask subtask3 = taskManager.createSubtask("Subtask 3", "Description 3", epic2.getId());
+
+        System.out.println("All Tasks: " + taskManager.getTasks());
+        System.out.println("All Epics: " + taskManager.getEpics());
+        System.out.println("All Subtasks: " + taskManager.getSubtasks());
+
+        task1.setStatus(TaskStatus.DONE);
+        subtask1.setStatus(TaskStatus.DONE);
+        subtask2.setStatus(TaskStatus.DONE);
+        subtask3.setStatus(TaskStatus.IN_PROGRESS);
+
+        System.out.println("Updated Tasks: " + taskManager.getTasks());
+        System.out.println("Updated Epics: " + taskManager.getEpics());
+        System.out.println("Updated Subtasks: " + taskManager.getSubtasks());
+
+        taskManager.deleteTaskById(task1.getId());
+        taskManager.deleteTaskById(epic1.getId());
+
+        System.out.println("After Deletion - All Tasks: " + taskManager.getTasks());
+        System.out.println("After Deletion - All Epics: " + taskManager.getEpics());
+        System.out.println("After Deletion - All Subtasks: " + taskManager.getSubtasks());
     }
 }
