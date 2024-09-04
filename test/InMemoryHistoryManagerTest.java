dea75bf3ab9bb1fcaf5807c50423fbd8a317ea5e
ChakNikolay87 <ChakNikolay@yandex.ru>
2024-09-04 10:43:00 +0300

    changed the location of the folder path

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import history.HistoryManager;
 import managers.Managers;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,7 +6,8 @@ import tasks.Task;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InMemoryHistoryManagerTest {
 
