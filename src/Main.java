d0a91ca95dff4bfd79a58bd74f15006ac3f25a5e
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-09-16 12:31:18 +0300

    Update Main.java
    made changes

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,17 @@ public class Main {
         try {
             File tempFile = File.createTempFile("tasks", ".csv");
             tempFile.deleteOnExit();
-            
             FileBackedTaskManager manager = new FileBackedTaskManager(tempFile);
-            
             Task task1 = new Task(0, "Task 1", "Description 1", TaskStatus.NEW);
             Task task2 = new Task(1, "Task 2", "Description 2", TaskStatus.IN_PROGRESS);
             manager.createTask(task1);
             manager.createTask(task2);
-            
+           
             Epic epic1 = new Epic(0, "Epic 1", "Description 1", TaskStatus.NEW);
             Epic epic2 = new Epic(1, "Epic 2", "Description 2", TaskStatus.IN_PROGRESS);
             manager.createEpic(epic1);
             manager.createEpic(epic2);
-            
+           
             Subtask subtask1 = new Subtask(0, "Subtask 1", "Description 1", epic1.getId(), TaskStatus.DONE);
             Subtask subtask2 = new Subtask(1, "Subtask 2", "Description 2", epic1.getId(), TaskStatus.IN_PROGRESS);
             Subtask subtask3 = new Subtask(2, "Subtask 3", "Description 3", epic2.getId(), TaskStatus.NEW);
@@ -33,11 +31,9 @@ public class Main {
             manager.createSubtask(subtask3);
             
             FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(tempFile);
-            
             System.out.println("Loaded tasks: " + loadedManager.getTasks());
             System.out.println("Loaded epics: " + loadedManager.getEpics());
             System.out.println("Loaded subtasks: " + loadedManager.getSubtasks());
-
         } catch (IOException e) {
             System.err.println("An error occurred while handling the file: " + e.getMessage());
         }
