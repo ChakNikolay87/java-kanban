07feb76947b398b0fdae3fe3fc06b4818149bb69
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-09-22 10:12:42 +0300

    Update Task.java
    Внес изменения

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,5 +1,7 @@
 package tasks;
 
+import managers.TaskType;
+
 import java.util.Objects;
 
 public class Task {
@@ -8,11 +10,11 @@ public class Task {
     private String description;
     private TaskStatus status;
 
-    public Task(int id, String name, String description) {
+    public Task(int id, String name, String description, TaskStatus status) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.status = TaskStatus.NEW;
+        this.status = status;
     }
 
     public int getId() {
@@ -60,13 +62,30 @@ public class Task {
         return Objects.hash(id, name, description, status);
     }
 
-    @Override
     public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", status=" + status +
-                '}';
+        return String.format("%d,%s,%s,%s,%s", id, TaskType.TASK, name, status, description);
     }
+    
+    public String taskToString() {
+        return String.format("%d,%s,%s,%s,%s", getId(), TaskType.TASK.name(), getName(), getStatus(), getDescription());
+    }
+
+    public static Task fromString(String value) {
+        String[] fields = value.split(",");
+        int id = Integer.parseInt(fields[0]);
+        TaskType taskType = TaskType.valueOf(fields[1]);
+        String name = fields[2];
+        TaskStatus status = TaskStatus.valueOf(fields[3]);
+        String description = fields[4];
+
+        if (taskType == TaskType.TASK) {
+            return new Task(id, name, description, status);
+        }
+        throw new IllegalArgumentException("Неподдерживаемый тип задачи: %s" + taskType);
+    }
+
+    public String type() {
+        return TaskType.TASK.name();
+    }
+
 }
