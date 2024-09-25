af7f66592027944395b96293ac358bd773052f63
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-09-25 12:33:16 +0300

    Update ManagerSaveException.java
    Внес изменения

--- src/managers/ManagerSaveException.java
+++ src/managers/ManagerSaveException.java
@@ -1,8 +1,7 @@
-package managers;
+package manager;
 
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException(String message, Throwable cause) {
-        super(message, cause);
+    public ManagerSaveException(String message) {
+        super(message);
     }
 }
-
