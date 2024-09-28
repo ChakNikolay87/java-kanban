cd8445f1e57786b0071f06adf98b86c5e61fe7e5
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-09-28 17:24:57 +0300

    Update Task.java
    Внес изменения

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -2,7 +2,6 @@ package tasks;
 
 import managers.TaskType;
 import status.Status;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -118,23 +117,6 @@ public class Task {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
         return String.format("%d,%s,%s,%s,%s,%d,%s", id, TaskType.TASK, name, status, description, duration.toMinutes(),
                 startTime.format(formatter));
-    }
-
-    public static Task fromString(String value) {
-        String[] fields = value.split(",");
-        int id = Integer.parseInt(fields[0]);
-        TaskType taskType = TaskType.valueOf(fields[1]);
-        String name = fields[2];
-        Status status = Status.valueOf(fields[3]);
-        String description = fields[4];
-        long durationMinutes = Long.parseLong(fields[5]);
-        Duration duration = Duration.ofMinutes(durationMinutes);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
-        LocalDateTime startTime = LocalDateTime.parse(fields[6], formatter);
-        if (taskType == TaskType.TASK) {
-            return new Task(name, description, id, status, duration, startTime);
-        }
-        throw new IllegalArgumentException(String.format("Неподдерживаемый тип задачи: %s", taskType));
     }
 
     public LocalDateTime getEndTime() {
