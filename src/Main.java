a26764c7c4ab1751278245380b70e1cc64db6cd5
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-09-30 10:22:56 +0300

    Update Main.java
    Внес изменения

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,26 @@ import tasks.Epic;
 import tasks.Subtask;
 import tasks.Task;
 import status.Status;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner;
-
     public static void main(String[] args) {
         TaskManager taskManager = Managers.getDefault();
-        Task task1 = new Task("Переезд", "Собрать вещи", Duration.ofMinutes(60),
-        TaskManager inMemoryTaskManager = Managers.getDefault();
-
         Task task1 = new Task("Переезд", "Собрать вещи", Duration.ofMinutes(60),
                 LocalDateTime.of(2024, 9, 23, 10, 20));
         Task task2 = new Task("Стрижка", "Сходить в барбершоп", Duration.ofHours(3),
                 LocalDateTime.of(2024, 9, 24, 17, 0));
-
         Epic epic1 = new Epic("Чертежи моста", "Сделать проект моста через реку Волга");
         Epic epic2 = new Epic("Командировка", "Подготовиться к командировке");
-
         Subtask subtask11 = new Subtask("Пролетное строение", "Начертить пролетное строение", 3,
                 Duration.ofDays(14), LocalDateTime.of(2024, 10, 13, 8, 0));
         Subtask subtask12 = new Subtask("Опоры", "Начертить опоры", 3,
                 Duration.ofDays(8), LocalDateTime.of(2024, 10, 28, 8, 0));
         Subtask subtask21 = new Subtask("Билеты на самолет", "Купить билеты на самолет", 4,
                 Duration.ofMinutes(10), LocalDateTime.of(2024, 11, 25, 14, 5));
-        Subtask subtask11 = new Subtask("Пролетное строение",
-                "Начертить пролетное строение",
-                3,
-                Duration.ofDays(14),
-                LocalDateTime.of(2024, 10, 13, 8, 0));
-        Subtask subtask12 = new Subtask("Опоры",
-                "Начертить опоры",
-                3,
-                Duration.ofDays(8),
-                LocalDateTime.of(2024, 10, 28, 8, 0));
-        Subtask subtask21 = new Subtask("Билеты на самолет",
-                "Купить билеты на самолет",
-                4,
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2024, 11, 25, 14, 5));
 
         scanner = new Scanner(System.in);
 
@@ -65,11 +43,6 @@ public class Main {
                 case "3":
                     taskManager.clearTasks();
                     System.out.println("Все задачи удалены.");
-                    System.out.println(inMemoryTaskManager.getTasks().values());
-                    break;
-                case "3":
-                    taskManager.clearTasks();
-                    System.out.println("Все задачи удалены.");
                     break;
                 case "4":
                     System.out.println(taskManager.getTask(task1.getId()).orElse(null));
@@ -79,11 +52,6 @@ public class Main {
                             task1.getId(), Status.DONE, Duration.ofMinutes(60),
                             LocalDateTime.of(2024, 9, 23, 10, 20));
                     taskManager.updateTask(updatedTask);
-                    System.out.println("Задача обновлена.");
-                    break;
-                case "6":
-                    taskManager.deleteTask(task1.getId());
-                    inMemoryTaskManager.updateTask(task3);
                     System.out.println("Задача обновлена.");
                     break;
                 case "6":
@@ -100,10 +68,6 @@ public class Main {
                     break;
                 case "9":
                     taskManager.clearEpics();
-                    System.out.println(inMemoryTaskManager.getEpics().values());
-                    break;
-                case "9":
-                    taskManager.clearEpics();
                     System.out.println("Все эпики удалены.");
                     break;
                 case "10":
@@ -113,14 +77,6 @@ public class Main {
                     Epic updatedEpic = new Epic("Чертежи нового арочного моста",
                             "Сделать часть нового проекта Волга", epic1.getId());
                     taskManager.updateEpic(updatedEpic);
-                    System.out.println("Эпик обновлен.");
-                    break;
-                case "12":
-                    taskManager.deleteEpic(epic1.getId());
-                    Epic epic3 = new Epic("Чертежи нового арочного моста",
-                            "Сделать часть нового проекта Волга",
-                            epic1.getId());
-                    inMemoryTaskManager.updateEpic(epic3);
                     System.out.println("Эпик обновлен.");
                     break;
                 case "12":
@@ -142,14 +98,6 @@ public class Main {
                     break;
                 case "16":
                     System.out.println(taskManager.getSubtaskById(subtask11.getId()).orElse(null));
-                    System.out.println(inMemoryTaskManager.getSubtasks().values());
-                    break;
-                case "15":
-                    taskManager.clearSubtasks();
-                    System.out.println("Все подзадачи удалены.");
-                    break;
-                case "16":
-                    System.out.println(taskManager.getSubtaskById(subtask11.getId()).orElse(null));
                     break;
                 case "17":
                     Subtask updatedSubtask = new Subtask(subtask11.getId(), "Пролетное строение",
@@ -157,16 +105,6 @@ public class Main {
                             subtask11.getSubtasksEpicId(), subtask11.getDuration(),
                             subtask11.getStartTime());
                     taskManager.updateSubtask(updatedSubtask);
-                    System.out.println("Подзадача обновлена.");
-                    break;
-                case "18":
-                    taskManager.deleteSubtask(subtask11.getId());
-                    System.out.println("Подзадача удалена.");
-                    break;
-                case "19":
-                    System.out.println(taskManager.getSubtasksOfEpic(epic1));
-                    System.out.println(taskManager.getSubtasksOfEpic(epic2));
-                    inMemoryTaskManager.updateSubtask(subtask13);
                     System.out.println("Подзадача обновлена.");
                     break;
                 case "18":
