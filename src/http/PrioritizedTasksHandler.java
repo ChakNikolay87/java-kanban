200034ecf66fff076f3b79a41aa5163ac41d88b1
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-10-13 10:25:54 +0300

    Update PrioritizedTasksHandler.java
    Внес изменения

--- src/http/PrioritizedTasksHandler.java
+++ src/http/PrioritizedTasksHandler.java
@@ -2,32 +2,38 @@ package http;
 
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-import http.BaseHttpHandler;
-import http.GsonUtil;
 import managers.TaskManager;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrioritizedTasksHandler extends BaseHttpHandler {
+    private static final Logger logger = Logger.getLogger(PrioritizedTasksHandler.class.getName());
     private final TaskManager taskManager;
     private final Gson gson;
 
     public PrioritizedTasksHandler(TaskManager taskManager) {
         this.taskManager = taskManager;
-        this.gson = GsonUtil.createGson();  // Use the custom Gson with adapters
+        this.gson = GsonUtil.createGson();
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try {
             if ("GET".equals(exchange.getRequestMethod())) {
+                logger.info("Received GET request for prioritized tasks");
+
                 String response = gson.toJson(taskManager.getPrioritizedTasks());
                 sendText(exchange, response);
+
+                logger.info("Prioritized tasks sent successfully");
             } else {
+                logger.warning("Unsupported HTTP method: " + exchange.getRequestMethod());
                 sendNotFound(exchange);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Error handling request", e);
             sendInternalError(exchange, "Internal server error: " + e.getMessage());
         }
     }
